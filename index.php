0add0168336f3b129c7c80fc2dbdebd47f5a171d
Lemuel <31605026+lemuelduragos@users.noreply.github.com>
2017-10-19 15:36:53 +0800

    hook

--- index.php
+++ index.php
@@ -1,81 +1,171 @@
 <?php
-# Array of the authorized IP addresses who can POST here. You can override this in your config if you so choose.
-$authorized_ips = array(
-	'122.3.121.207',
-	'50.57.128.197',
-	'108.171.174.178',
-	'50.57.231.61',
-	'54.235.183.49',
-	'54.235.183.23',
-	'54.235.118.251',
-	'54.235.120.57',
-	'54.235.120.61',
-	'54.235.120.62'
+/*
+ *	GitHub & Bitbucket Deployment Sample Script
+ *	Originally found at
+ *	http://brandonsummers.name/blog/2012/02/10/using-bitbucket-for-automated-deployments/
+ *	http://jonathannicol.com/blog/2013/11/19/automated-git-deployments-from-bitbucket/
+ *  
+ *	We assume you did a 'git clone --mirror' to your local repo directory,
+ *	And then, 'GIT_WORK_TREE=[www directory] git checkout -f [your desired branch]'  
+ *  
+  */
+/*
+ *    Webhook Sample
+ *        For BitBucket, use 'POST HOOK'
+ *        For GitHub, use 'Webhooks'
+ *    URL Format
+ *        https://[Basic Auth ID]:[Basic Auth Pass]@example.com/deployments.php?key=EnterYourSecretKeyHere
+ *        (https access & Basic Auth makes it a bit more secure if your server supports it)
+ */
+/**
+* The TimeZone format used for logging.
+* @var Timezone
+* @link    http://php.net/manual/en/timezones.php
+*/
+date_default_timezone_set('Asia/Tokyo');
+/**
+* The Secret Key so that it's a bit more secure to run this script
+* 
+* @var string
+*/
+$secret_key = 'duragos';
+/**
+* The Options
+* Only 'directory' is required.
+* @var array
+*/
+$options = array(
+    'directory'     => '/var/www/html/hook-duragos/Webhook',
+    'work_dir'      => '/var/www/html/deploy-duragos',  // leave it blank or null if you are using .git directory
+    'log'           => 'deploy_log_filename.log',
+    'branch'        => 'master',
+    'remote'        => 'origin',
+    'date_format'   => 'Y-m-d H:i:sP',
+    'syncSubmodule' => false,
+    'git_bin_path'  => 'git',
 );
-# Put your deploy config file in the same dir as this file
-if ( file_exists( dirname( __FILE__ ) . '/deploy-config.php' ) )
-	include_once( 'deploy-config.php' );
-# A regex matching the ref of the "push". `git pull` will only run if this matches. Default is the master branch.
-if ( !defined( 'REF_REGEX' ) )
-	define( 'REF_REGEX', '#^refs/heads/master$#' );
-# Log location; make sure it exists
-if ( !defined( 'LOG' ) )
-	define( 'LOG', '../logs/deploy.log' );
-# Where is your repo directory? This script will chdir to it. If %s is present, it gets replaced with the repository name
-if ( !defined( 'REPO_DIR' ) )
-	define( 'REPO_DIR', dirname( __FILE__ ) . "/wp-content/themes/%s/" );
-# Where is your git binary, and what command would you like to run?
-if ( !defined( 'GIT_COMMAND' ) )
-	define( 'GIT_COMMAND', 'git pull' );
-# Do we want to do IP verification?
-if ( !defined( 'VERIFY_IP' ) )
-	define( 'VERIFY_IP', true );
-# If defined, $_POST gets logged
-# define( 'DUMP_POSTDATA', true );
-# In your webhook URL to github, you can append ?auth={{ this field }} as a very simple gut-check authentication.
-# define( 'AUTH_KEY', 'whatever-you-want' );
-if ( is_writable( LOG ) && $handle = fopen( LOG, 'a' ) ) {
-	# Sweet taste of victory
-	fwrite( $handle, date( 'Y-m-d H:i:s' ) . "\n==============================\n" );
-} else {
-	@fclose( $handle );
-	header( $_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500 );
-	die( 'Please complete installation' );
+if ($_GET['key'] === $secret_key)  {
+    $deploy = new Deploy($options);
+	$deploy->execute();
+    /*
+    $deploy->post_deploy = function() use ($deploy) {
+      // hit the wp-admin page to update any db changes
+       exec('curl http://example.com/wp-admin/upgrade.php?step=upgrade_db');
+       $deploy->log('Updating wordpress database... ');
+    };
+    */
 }
-# Do some authentication
-if ( defined( 'AUTH_KEY' ) && ( !isset( $_GET['auth'] ) || AUTH_KEY != $_GET['auth'] ) ) {
-	$error = "Auth key doesn't match";
-} elseif ( !isset( $_POST['payload'] ) ) {
-	$error = '$_POST["payload"] is not set';
-} elseif ( VERIFY_IP && !in_array( $_SERVER['REMOTE_ADDR'], $authorized_ips ) ) {
-	$error = "{$_SERVER['REMOTE_ADDR']} is not authorized. Authorized IPs are: " . implode( ', ', $authorized_ips );
-} else {
-	$error = false;
+class Deploy {
+    /**
+    * A callback function to call after the deploy has finished.
+    * 
+    * @var callback
+    */
+    public $post_deploy;
+    
+    /**
+    * The name of the file that will be used for logging deployments. Set to 
+    * FALSE to disable logging.
+    * 
+    * @var string
+    */
+    private $_log = 'deploy.log';
+    
+    /**
+    * The timestamp format used for logging.
+    * 
+    * @link    http://www.php.net/manual/en/function.date.php
+    * @var     string
+    */
+    private $_date_format = 'Y-m-d H:i:sP';
+    
+    /**
+    * The path to git
+    * 
+    * @var string
+    */
+    private $_git_bin_path = 'git';
+    /**
+    * The directory where your git repository is located, can be 
+    * a relative or absolute path from this PHP script on server.
+    * 
+    * @var string
+    */
+    private $_directory;
+    /**
+    * The directory where your git work directory is located, can be 
+    * a relative or absolute path from this PHP script on server.
+    * 
+    * @var string
+    */
+    private $_work_dir;
+    /**
+    * Sets up defaults.
+    * 
+    * @param  array   $option       Information about the deployment
+    */
+    public function __construct($options = array())
+    {
+        $available_options = array('directory', 'work_dir', 'log', 'date_format', 'branch', 'remote', 'syncSubmodule, git_bin_path');
+    
+        foreach ($options as $option => $value){
+            if (in_array($option, $available_options)) {
+                $this->{'_'.$option} = $value;
+                if ($option == 'directory' || $option == 'work_dir') {
+                    // Determine the directory path
+                    $this->{'_'.$option} = realpath($value).DIRECTORY_SEPARATOR;
+                }
+            }
+        }
+        if (empty($this->_work_dir)) {
+            $this->_work_dir = $this->_directory;
+            $this->_directory = $this->_directory . '/.git';
+        }
+    
+        $this->log('Attempting deployment...');
+        $this->log('Git Directory:' . $this->_directory);
+        $this->log('Work Directory:' . $this->_work_dir);
+    }
+    /**
+    * Writes a message to the log file.
+    * 
+    * @param  string  $message  The message to write
+    * @param  string  $type     The type of log message (e.g. INFO, DEBUG, ERROR, etc.)
+    */
+    public function log($message, $type = 'INFO')
+    {
+        if ($this->_log) {
+            // Set the name of the log file
+            $filename = $this->_log;
+            if ( ! file_exists($filename)) {
+                // Create the log file
+                file_put_contents($filename, '');
+                // Allow anyone to write to log files
+                chmod($filename, 0666);
+            }
+            // Write the message into the log file
+            // Format: time --- type: message
+            file_put_contents($filename, date($this->_date_format).' --- '.$type.': '.$message.PHP_EOL, FILE_APPEND);
+        }
+    }
+    /**
+    * Executes the necessary commands to deploy the website.
+    */
+    public function execute()
+    {
+        try {
+            // Update the local repository
+            exec($this->_git_bin_path . ' --git-dir=' . $this->_directory . '/.git --work-tree=' . $this->_work_dir . ' fetch', $output);
+            $this->log('Fetching changes... '.implode(' ', $output));
+            // Checking out to web directory
+            exec('cd ' . $this->_directory . ' && GIT_WORK_TREE=' . $this->_work_dir . ' ' . $this->_git_bin_path  . ' checkout -f', $output);
+            $this->log('Checking out changes to www directory... '.implode(' ', $output));
+            if (is_callable($this->post_deploy)) {
+                call_user_func($this->post_deploy, $this->_data);
+            }
+            $this->log('Deployment successful.');
+        } catch (Exception $e) {
+            $this->log($e, 'ERROR');
+        }
+    }
 }
-if ( false !== $error ) {
-	fwrite( $handle, "*** ALERT ***\nFailed attempt to access deployment script!\n\nMESSAGE: $error\n\n" . print_r( $_SERVER, 1 ) . print_r( $_REQUEST, 1 ) . "\n\n\n" );
-	@fclose( $handle );
-    header( $_SERVER['SERVER_PROTOCOL'] . ' 401 Unauthorized', true, 401 );
-	die( "You don't have permission to access this page." );
-}
-# We're authorized, let's do this!
-$content = '';
-if ( defined( 'DUMP_POSTDATA' ) )
-	$content .= print_r( $_POST, 1 ) . "\n\n";
-if ( false === fwrite( $handle, $content ) ) {
-	echo "Couldn't write to log!\n";
-}
-$payload = json_decode( $_POST['payload'] );
-if ( preg_match( REF_REGEX, $payload->ref ) ) {
-	# If we have a commit to master, we can pull on it
-	$output = array( 'bash> ' . GIT_COMMAND );
-	chdir( sprintf( REPO_DIR, $payload->repository->name ) );
-	exec( GIT_COMMAND . ' 2>&1', $output );
-	fwrite( $handle, "`$payload->ref` matches, executing:\n" . GIT_COMMAND . "\n" . implode( "\n", $output ) . "\n" );
-} else {
-	fwrite( $handle, "`$payload->ref` doesn't match the ref criteria\n" );
-}
-fwrite( $handle, "Over and out!\n\n\n" );
-@fclose( $handle );
-//how
-?>
