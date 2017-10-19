eb96ce2b998b8017bb86050f9d3ba21cfed46ca1
Lemuel <31605026+lemuelduragos@users.noreply.github.com>
2017-10-19 16:23:22 +0800

    test again

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-// Use in the "Post-Receive URLs" section of your GitHub repo.
 if ( $_POST['payload'] ) {
   shell_exec( 'cd /var/www/html/deploy-duragos/Webhook && git reset --hard HEAD && git pull' );
 }
-?>hello
+?>banana
