49f6428c2690164eafad549bc70174c18991cc52
Lemuel <31605026+lemuelduragos@users.noreply.github.com>
2017-10-20 09:39:21 +0800

    hello

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
     $commands = array(
         'echo $PWD',
         'whoami',
+        'cd /var/www/html/deploy-duragos/Webhook',
         'sudo git pull',
     );
     // Run the commands for output
